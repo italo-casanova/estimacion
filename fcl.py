66fd332af1e5fbe9c32bcaa07db3c6a240b631e5
Italo <italo.casanova.d@uni.pe>
2023-12-06 11:28:00 -0500

    udpate

--- fcl.py
+++ fcl.py
@@ -1,32 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 n_escenarios = 1000
 n_años = 5
 
 flujos_caja_libre = np.zeros((n_escenarios, n_años))
 
-for i in range(n_escenarios):
-    ingresos_simulados = np.random.normal(
-            np.mean(datos_resultados['Ingresos']),
-            np.std(datos_resultados['Ingresos']),
-            n_años)
+def calculo_flujo_caja_libre(ingresos, costos_operativos, capex):
 
-    costos_operativos_simulados = np.random.normal(
-            np.mean(datos_resultados['Costos_Operativos']),
-            np.std(datos_resultados['Costos_Operativos']),
-            n_años)
+    for i in range(n_escenarios):
+        ingresos_simulados = np.random.normal(
+                np.mean(datos_resultados['Ingresos']),
+                np.std(datos_resultados['Ingresos']),
+                n_años)
 
-    capex_simulado = np.random.normal(
-            np.mean(datos_balance['CapEx']),
-            np.std(datos_balance['CapEx']),
-            n_años)
+        costos_operativos_simulados = np.random.normal(
+                np.mean(datos_resultados['Costos_Operativos']),
+                np.std(datos_resultados['Costos_Operativos']),
+                n_años)
 
-    ebitda = ingresos_simulados - costos_operativos_simulados
-    fcl = ebitda - capex_simulado
+        capex_simulado = np.random.normal(
+                np.mean(datos_balance['CapEx']),
+                np.std(datos_balance['CapEx']),
+                n_años)
 
-    flujos_caja_libre[i] = fcl
+        ebitda = ingresos_simulados - costos_operativos_simulados
+        fcl = ebitda - capex_simulado
+
+        flujos_caja_libre[i] = fcl
+
 
 tasa_descuento = 0.05
 
